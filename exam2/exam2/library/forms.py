9d12504eb86bebd566e6a37d82fdc667684e9670
karalkal <karalkal@yahoo.co.uk>
2022-02-26 05:26:18 +0000

    added placeholders to forms, removed closing semicolumns of labels

--- exam2/exam2/library/forms.py
+++ exam2/exam2/library/forms.py
@@ -4,15 +4,61 @@ from exam2.library.models import Profile, Book
 
 
 class CreateProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""  # Removes : as label suffix
+
     class Meta:
         model = Profile
         fields = '__all__'
+        labels = {
+            'first_name': "First Name",
+            'last_name': "Last name",
+            'image_url': "Image URL",
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={'placeholder': "First Name",
+                       }),
+
+            'last_name': forms.TextInput(
+                attrs={'placeholder': "Last Name",
+                       }),
+
+            'image_url': forms.URLInput(
+                attrs={'placeholder': "URL",
+                       }),
+        }
 
 
 class EditProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""  # Removes : as label suffix
+
     class Meta:
         model = Profile
         fields = '__all__'
+        labels = {
+            'first_name': "First Name",
+            'last_name': "Last name",
+            'image_url': "Image URL",
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={'placeholder': "First Name",
+                       }),
+
+            'last_name': forms.TextInput(
+                attrs={'placeholder': "Last Name",
+                       }),
+
+            'image_url': forms.URLInput(
+                attrs={'placeholder': "URL",
+                       }),
+        }
 
 
 class DeleteProfileForm(forms.ModelForm):
@@ -27,12 +73,25 @@ class DeleteProfileForm(forms.ModelForm):
 
 
 class CreateBookForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""  # Removes : as label suffix
+
     class Meta:
         model = Book
         fields = '__all__'
+        labels = {
+            'first_name': "First Name",
+            'last_name': "Last name",
+            'image_url': "Image URL",
+        }
 
 
 class EditBookForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""  # Removes : as label suffix
+
     class Meta:
         model = Book
         fields = '__all__'
